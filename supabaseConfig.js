7d07c452c063ea3401e8490a3ff58b427d492404
Sarmad Tabassum <sarmadplaying@gmail.com>
2026-06-12 09:18:20 +0000

    'ok'

---
+++ supabaseConfig.js
@@ -0,0 +1,4 @@
+const supabaseApi = 'sb_publishable_T_i7VOUOOhhwD2hzzVR6hQ_Mawxjknc'
+const supabaseUrl = 'https://rwzyvusziktwgfudyoiy.supabase.co'
+
+export const supabaseClient = supabase.createClient(supabaseUrl, supabaseApi)
